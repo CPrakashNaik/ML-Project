2fc8688cedf3e7577e5238c684947f67d8d50caa
CPrakashNaik <162329916+CPrakashNaik@users.noreply.github.com>
2025-08-22 15:08:08 +0530

    second commit

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='mlproject',
 version='0.0.1',
-author='soahil',
-author_email='sohail007@gmail.com',
+author='naik',
+author_email='cprakashnaik2004@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
